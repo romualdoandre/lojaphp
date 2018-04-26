cad3b1e37b2ba68e84f3ee68d1c234c6d6800093
romualdo <romualdoandre@gmail.com>
2018-04-25 22:35:12 -0300

    lalaa

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,13 +4,18 @@ namespace Hcode\Model;
 
 use \Hcode\Model;
 use \Hcode\DB\Sql;
+use \Hcode\Mailer;
 
 class User extends Model {
 
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_Secret";
+	const ERROR = "UserError";
+	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSucesss";
 
 	protected $fields = [
-		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister"
+		"iduser", "idperson", "deslogin", "despassword", "inadmin", "dtergister","desperson","desemail","nrphone"
 	];
 
 	public static function login($login, $password):User
@@ -65,11 +70,107 @@ class User extends Model {
 			(bool)$_SESSION[User::SESSION]["iduser"] !== $inadmin
 		) {
 			
-			header("Location: /admin/login");
+			header("Location: admin/login");
 			exit;
 
 		}
 
+	}
+	
+	public static function listAll(){
+		$db = new Sql();
+
+		return $db->select("SELECT * FROM tb_users a inner join tb_persons b using(idperson) order by b.desperson");
+	}
+	
+	public function save(){
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_users_save(:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+		$this->setData($results[0]);
+		
+	}
+	public static function getPasswordHash($password)
+	{
+		return password_hash($password, PASSWORD_DEFAULT, [
+			'cost'=>12
+		]);
+	}
+
+	public function get($iduser){
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_users a INNER JOIN tb_persons b on a.idperson=b.idperson WHERE a.iduser = :iduser", array(
+			":iduser"=>$iduser
+		));
+		$data = $results[0];
+		
+		$this->setData($data);
+	}
+	
+	public function update(){
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser,:desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+		$this->setData($results[0]);
+		
+	}
+	
+	public function delete(){
+		$sql = new Sql();
+		$results = $sql->select("CALL sp_users_delete(:iduser)", array(
+			":iduser"=>$this->getiduser()
+		));
+	}
+	
+	public static function getForgot($email){
+		$sql=new Sql();
+		$results=$sql->select("select * from tb_persons a inner join tb_users b on a.idperson=b.idperson where a.desemail=:email",array(
+		":email"=>$email
+		));
+		if(count($results)===0){
+			throw new \Exception("Não foi possível recuperar a senha");
+		}
+		else{
+			$data=$results[0];
+			$results2=$sql->select("call  sp_userspasswordsrecoveries_create(:iduser,:desip)",array(
+				":iduser"=>$data["user"],
+				">desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+			if(count($results2)===0){
+				throw new \Exception("Não foi possível recuperar a senha");
+			}
+			else{
+				$dataRecovery=$results2[0];
+				$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+				if ($inadmin === true) {
+					
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				} else {
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+				}
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir Senha da Hcode Store", "forgot", array(
+					"name"=>$data["desperson"],
+					"link"=>$link
+				));
+				$mailer->send();
+				return $data;
+
+			}
+		}
+		
 	}
 
 }
--- views/admin/login.html
+++ views/admin/login.html
@@ -57,7 +57,7 @@
         <!-- /.col -->
       </div>
     </form>
-    <a href="#">I forgot my password</a><br>
+    <a href="/admin/forgot">I forgot my password</a><br>
     <a href="register.html" class="text-center">Register a new membership</a>
 
   </div>
