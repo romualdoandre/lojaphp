913e0a456c01091c35e0388c6282ac847fe5da6a
romualdo <romualdoandre@gmail.com>
2019-05-29 22:32:43 -0300

    Detalhes do produto

--- site.php
+++ site.php
@@ -33,3 +33,13 @@ $app->get("/categories/:idcategory",function($idcategory){
 		'pages'=>$pages
 	]);
 });
+
+$app->get("/products/:desurl",function($desurl){
+	$product = new Product();
+	$product->getFromURL($desurl);
+	$page = new Page();
+	$page->setTpl("product-detail",[
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()]);
+
+});
