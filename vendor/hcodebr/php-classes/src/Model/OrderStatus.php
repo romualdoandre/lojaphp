6c0aeaa24b34fe2ab0a513d1168b0b0b4feb0714
romualdo <romualdoandre@gmail.com>
2019-08-06 15:32:35 -0300

    boleto

---
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class OrderStatus extends Model {
+
+	const EM_ABERTO = 1;
+	const AGUARDANDO_PAGAMENTO = 2;
+	const PAGO = 3;
+    const ENTREGUE = 4;
+    protected $fields = [
+		"idstatus", "desstatus",'dtregister'
+	];
+
+	public static function listAll()
+	{
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus");
+
+	}
+
+}
+
+?>
